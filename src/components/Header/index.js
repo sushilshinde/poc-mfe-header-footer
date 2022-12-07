93606c7333eb8cc5628f3875a27b52794777c2d3
Aswathi Suseelan <aswathi.suseelan@AD.MSYSTECHNOLOGIES.COM>
2022-12-07 16:46:30 +0530

    added component description

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
 import "./index.css";
 
+/**
+ * Header With Searchbar
+ *
+ * @description: Shows a header component with a search bar to input a github username
+ * @returns Header Component
+ */
 function Header() {
     const [searchValue, setSearchValue] = useState("");
     const handleSearch = () => {
-        const event = new CustomEvent('getSearchTerm', { detail: searchValue });
+        const event = new CustomEvent('getSearchTerm', { detail: searchValue.trim() });
 		window.dispatchEvent(event)
     };
     return (
@@ -15,6 +21,7 @@ function Header() {
                         type="search"
                         id="form1"
                         className="form-control"
+                        placeholder="Github Username"
                         onChange={(e) => setSearchValue(e.target.value)}
                     />
                 </div>
@@ -22,6 +29,7 @@ function Header() {
                     type="button"
                     className="btn btn-primary col-1"
                     onClick={handleSearch}
+                    disabled={searchValue.trim().length === 0}
                 >
                     <i className="fa fa-search" aria-hidden="true"></i>
                 </button>
